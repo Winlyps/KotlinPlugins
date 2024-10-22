ac4aa646c0e77e3379068bc140b727cadf87bed4
Winlypse <nts3@o2.pl>
2024-10-22 18:50:54 +0200

    Now you can execute command in console + Fixed permissions issue

--- src/main/kotlin/winlyps/kotlinPlugins/KotlinPluginCommand.kt
+++ src/main/kotlin/winlyps/kotlinPlugins/KotlinPluginCommand.kt
@@ -9,10 +9,6 @@ import winlyps.kotlinPlugins.utils.PluginUtils
 class KotlinPluginCommand(private val plugin: KotlinPlugins) : CommandExecutor {
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        if (sender !is Player) {
-            sender.sendMessage("§cThis command can only be executed by a player.")
-            return true
-        }
 
         if (!sender.hasPermission("kotlinplugins.use")) {
             sender.sendMessage("§cYou do not have permission to use this command.")
